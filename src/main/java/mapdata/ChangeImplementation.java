e0a60b32059280d4047a1b45d42c36f5d5184c6d
Brucegyx <yexuan.gao@outlook.com>
2024-03-11 14:59:09 -0700

    refactor

--- src/main/java/mapdata/ChangeImplementation.java
+++ src/main/java/mapdata/ChangeImplementation.java
@@ -20,6 +20,8 @@ public class ChangeImplementation extends UnicastRemoteObject implements ChangeI
     private final static String MEMBERSHIP_SERVER_ADDR = "127.0.0.1";
     private final static int MEMBERSHIP_SERVER_PORT = 4410;
     private Map<String, String> membershipMap = new HashMap<>();
+    private String addr = "";
+    private int port = 0;
     public ChangeImplementation() throws RemoteException {
         super();
     }
@@ -62,86 +64,20 @@ public class ChangeImplementation extends UnicastRemoteObject implements ChangeI
                 response.append("keymap ").append(getMap(type).entrySet());
                 break;
             case "del":
-                // for each ip address, port in the membership list send ddel1 message
-                // getMap(type).remove(key);
-                //response.append("delete key=").append(key);
-                Boolean abort = false;
-                for (String ip : membershipMap.keySet()) {
-                    try {
-                        // send ddel1 message to ip:port
-                        String port = membershipMap.get(ip);
-                        // send ddel1 message to ip:port
-                        Socket socket = new Socket(ip, Integer.parseInt(port));
-                        System.out.println("Sending ddel1 message to " + ip + ":" + port);
-                        PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
-                        BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-                        out.println("ddel1 " + key);
-                        //if any server responds with ddelabort, then send ddelabort message to all servers from the membershipMap
-                        String serverAck = in.readLine();
-                        if (serverAck.equals("abort")) {
-                            abort = true;
-                            System.out.println("Received ddelabort message from " + ip + ":" + port);
-                            response.append("abort");
-                            sendAbortToAllServers(key);
-                        } 
-                        
-                        System.out.println(serverAck.equals("ok " + key));
-                        
-                        socket.close();
-                    } catch (Exception e) {
-                        e.printStackTrace();
-                
-                    }
-                }
-                //if all servers respond with ok, then send ddel2 message to all servers from the membershipMap 
-                if (!abort) {
-                    for (String ip : membershipMap.keySet()) {
-                        try {
-                            // send ddel1 message to ip:port
-                            String port = membershipMap.get(ip);
-                            // send ddel1 message to ip:port
-                            Socket socket = new Socket(ip, Integer.parseInt(port));
-                            System.out.println("Sending ddel2 message to " + ip + ":" + port);
-                            PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
-                            BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-                            out.println("ddel2 " + key);
-                            response.append(in.readLine());
-                            socket.close();
-                        } catch (Exception e) {
-                            e.printStackTrace();
-                    
-                        }
-                    }
-                }
+                String deleteResult = handleClientDelete(key, membershipMap);
+                response.append(deleteResult);
                 break;
             case "ddel1":
-                System.out.println("Received ddel1 message for key=" + key);
-                if (keyLocks.containsKey(key) && keyLocks.get(key).tryLock()){
-                    
-                    System.out.println("Locking key=" + key);
-                    keyLocks.computeIfAbsent(key, k -> new ReentrantLock()).lock();
-                    response.append("ok " + key);
-                } else {
-                   // abort the delete
-                    System.out.println("Received ddel1 message for key=" + key + ", locked by others, aborting");
-                    response.append("abort");
-                }
+                String ddel1Result = handleDeletePhaseOne(key);
+                response.append(ddel1Result);
                 break;
             case "ddel2":
-                System.out.println("Received ddel2 message for key=" + key + " deleting");
-                keyLocks.get(key).unlock();
-                getMap(type).remove(key);
-
-                keyLocks.remove(key);
-                response.append("delete key=").append(key);
+                String ddel2Result = handleDeletePhaseTwo(key, type);
+                response.append(ddel2Result);
                 break;
             case "ddelabort":
-                Lock lockedObject = keyLocks.get(key);
-                if (keyLocks.containsKey(key)){
-                    System.out.println("Received ddelabort message for key=" + key + " unlocking");
-                    lockedObject.unlock();
-                }
-                response.append("delete key=").append(key).append(" aborted");
+                String ddelabortResult = handleDeleteAbort(key);
+                response.append(ddelabortResult);
                 break;
             case "store":
                 List<String> entries = new ArrayList<>();
@@ -166,22 +102,120 @@ public class ChangeImplementation extends UnicastRemoteObject implements ChangeI
             }
         return response.toString();
     }
-        private void sendAbortToAllServers (String key) {
+    private void sendAbortToAllServers (String key) {
+        for (String ip : membershipMap.keySet()) {
+            try {
+                String port = membershipMap.get(ip);
+                // send ddelabort message to ip:port
+                Socket socket = new Socket(ip, Integer.parseInt(port));
+                System.out.println("Sending ddelabort message to " + ip + ":" + port);
+                PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+                BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                out.println("ddelabort " + key);
+                socket.close();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+    private String handleClientDelete(String key, Map<String, String> membershipMap) {
+        // for each ip address, port in the membership list send ddel1 message
+        // getMap(type).remove(key);
+        //response.append("delete key=").append(key);
+        Boolean abort = false;
+        String res = "";
+        for (String ip : membershipMap.keySet()) {
+            try {
+                // send ddel1 message to ip:port
+                String port = membershipMap.get(ip);
+                // send ddel1 message to ip:port
+                Socket socket = new Socket(ip, Integer.parseInt(port));
+                System.out.println("Sending ddel1 message to " + ip + ":" + port);
+                PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+                BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                out.println("ddel1 " + key);
+                //if any server responds with ddelabort, then send ddelabort message to all servers from the membershipMap
+                String serverAck = in.readLine();
+                if (serverAck.equals("abort")) {
+                    abort = true;
+                    System.out.println("Received abort message from " + ip + ":" + port);
+                    res = "Delete abort";
+                    sendAbortToAllServers(key);
+                } 
+                
+                System.out.println(serverAck.equals("ok " + key));
+                
+                socket.close();
+            } catch (Exception e) {
+                abort = true;
+                e.printStackTrace();
+                break;
+            }
+        }
+        //if all servers respond with ok, then send ddel2 message to all servers from the membershipMap 
+        if (!abort) {
             for (String ip : membershipMap.keySet()) {
                 try {
+                    // send ddel1 message to ip:port
                     String port = membershipMap.get(ip);
-                    // send ddelabort message to ip:port
+                    // send ddel1 message to ip:port
                     Socket socket = new Socket(ip, Integer.parseInt(port));
-                    System.out.println("Sending ddelabort message to " + ip + ":" + port);
+                    System.out.println("Sending ddel2 message to " + ip + ":" + port);
                     PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
                     BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-                    out.println("ddelabort " + key);
+                    out.println("ddel2 " + key);
+                    res = in.readLine();
                     socket.close();
                 } catch (Exception e) {
+                    res = "abort, but exceptions occurred while deleting key=" + key + " from other servers.";
                     e.printStackTrace();
+            
                 }
             }
         }
+        return res;
+    }
+
+    private String handleDeletePhaseOne(String key) {
+        String res = "";
+        System.out.println("Received ddel1 message for key=" + key);
+        if (keyLocks.containsKey(key) && keyLocks.get(key).tryLock()){
+            
+            System.out.println("Locking key=" + key);
+            //keyLocks.computeIfAbsent(key, k -> new ReentrantLock()).lock();
+            res = "ok " + key;
+        } else {
+            // abort the delete
+            System.out.println("Received ddel1 message for key=" + key + ", locked by others, aborting");
+            res = "abort";
+        }
+
+        return res;
+    }
+
+    private String handleDeletePhaseTwo(String key, String type) {
+        String res = "";
+        System.out.println("Received ddel2 message for key=" + key + " deleting");
+        keyLocks.get(key).unlock();
+        getMap(type).remove(key);
+
+        keyLocks.remove(key);
+        res = "delete key=" +key;
+
+        return res;
+    }
+
+    private String handleDeleteAbort(String key) {
+        String res = "";
+        Lock lockedObject = keyLocks.get(key);
+        if (keyLocks.containsKey(key)){
+            System.out.println("Received ddelabort message for key=" + key + " unlocking");
+            lockedObject.unlock();
+        }
+        res = "delete key=" +key +" aborted";
+        return res;
+
+    }
 
     private ConcurrentHashMap<String, String> getMap(String type) {
         if ("tcp".equals(type)) {
