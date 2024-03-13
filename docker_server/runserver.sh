8b879ea55179f957e7f6627511692d496e50ad93
lucaslu1626 <lucaslu1626@gmail.com>
2024-03-13 12:15:08 -0700

    final test

--- docker_server/runserver.sh
+++ docker_server/runserver.sh
@@ -3,10 +3,10 @@
 # java -jar GenericNode.jar
 
 #TCP Server - centralized node directory 
-java -jar GenericNode.jar ts 4410
+#java -jar GenericNode.jar ts 4410
 
 #TCP Server
-#java -jar GenericNode.jar ts 1234 172.17.0.2
+java -jar GenericNode.jar ts 1234 172.17.0.2
 
 
 
